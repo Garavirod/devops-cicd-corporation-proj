6af5dcca685a8aa112de77d79082af5528d4899d
Garavirod <rodrigogarciaavila26@gmail.com>
2025-06-11 17:50:30 -0600

    feat(terraform): Enable EC2 Jenkins module configuration

--- infra/terraform/infra-setup/main.tf
+++ infra/terraform/infra-setup/main.tf
@@ -112,17 +112,17 @@ module "ec2_nexus" {
 }
 
 // EC2 for jenkins
-# module "ec2_jenkins" {
-#   source = "./ec2"
-#   project_name = var.project_name
-#   environment = var.environment
-#   key_name = aws_key_pair.instance_key_pair.key_name
-#   subnet_id = module.networking.corporate_public_subnets_ids[0]
-#   ec2_instance_name = "jenkins"
-#   ec2_instance_ami = var.ec2_jenkins_instance_ami
-#   ec2_instance_type = var.ec2_jenkins_instance_type
-#   security_group_ids = [module.ecs_k8s_security_groups.sg_ec2_k8s_id]
-#   enable_public_ip = true
-#   ebs_volume_size = 30
-#   ebs_volume_type = "gp2"
-# }
+module "ec2_jenkins" {
+  source = "./ec2"
+  project_name = var.project_name
+  environment = var.environment
+  key_name = aws_key_pair.instance_key_pair.key_name
+  subnet_id = module.networking.corporate_public_subnets_ids[0]
+  ec2_instance_name = "jenkins"
+  ec2_instance_ami = var.ec2_jenkins_instance_ami
+  ec2_instance_type = var.ec2_jenkins_instance_type
+  security_group_ids = [module.ecs_k8s_security_groups.sg_ec2_k8s_id]
+  enable_public_ip = true
+  ebs_volume_size = 30
+  ebs_volume_type = "gp2"
+}
