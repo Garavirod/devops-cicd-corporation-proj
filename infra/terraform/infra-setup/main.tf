9ab293e8be3bdb5d1ad3f49da3413ad3e0399079
Garavirod <rodrigogarciaavila26@gmail.com>
2025-06-11 17:07:37 -0600

    feat(terraform): Un-comment and enable EC2 modules for Sonarqube and Nexus

--- infra/terraform/infra-setup/main.tf
+++ infra/terraform/infra-setup/main.tf
@@ -80,36 +80,36 @@ module "ec2_slave_2" {
   ebs_volume_type = "gp2"
 }
 
-# // EC2 For Sonarqube
-# module "ec2_sonarqube" {
-#   source = "./ec2"
-#   project_name = var.project_name
-#   environment = var.environment
-#   key_name = aws_key_pair.instance_key_pair.key_name
-#   subnet_id = module.networking.corporate_public_subnets_ids[0]
-#   ec2_instance_name = "sonarqube"
-#   ec2_instance_ami = var.ec2_sonarqube_instance_ami
-#   ec2_instance_type = var.ec2_sonarqube_instance_type
-#   security_group_ids = [module.ecs_k8s_security_groups.sg_ec2_k8s_id]
-#   enable_public_ip = true
-#   ebs_volume_size = 20
-#   ebs_volume_type = "gp2"
-# }
-# // EC2 For Nexus
-# module "ec2_nexus" {
-#   source = "./ec2"
-#   project_name = var.project_name
-#   environment = var.environment
-#   key_name = aws_key_pair.instance_key_pair.key_name
-#   subnet_id = module.networking.corporate_public_subnets_ids[0]
-#   ec2_instance_name = "nexus"
-#   ec2_instance_ami = var.ec2_nexus_instance_ami
-#   ec2_instance_type = var.ec2_nexus_instance_type
-#   security_group_ids = [module.ecs_k8s_security_groups.sg_ec2_k8s_id]
-#   enable_public_ip = true
-#   ebs_volume_size = 20
-#   ebs_volume_type = "gp2"
-# }
+// EC2 For Sonarqube
+module "ec2_sonarqube" {
+  source = "./ec2"
+  project_name = var.project_name
+  environment = var.environment
+  key_name = aws_key_pair.instance_key_pair.key_name
+  subnet_id = module.networking.corporate_public_subnets_ids[0]
+  ec2_instance_name = "sonarqube"
+  ec2_instance_ami = var.ec2_sonarqube_instance_ami
+  ec2_instance_type = var.ec2_sonarqube_instance_type
+  security_group_ids = [module.ecs_k8s_security_groups.sg_ec2_k8s_id]
+  enable_public_ip = true
+  ebs_volume_size = 20
+  ebs_volume_type = "gp2"
+}
+// EC2 For Nexus
+module "ec2_nexus" {
+  source = "./ec2"
+  project_name = var.project_name
+  environment = var.environment
+  key_name = aws_key_pair.instance_key_pair.key_name
+  subnet_id = module.networking.corporate_public_subnets_ids[0]
+  ec2_instance_name = "nexus"
+  ec2_instance_ami = var.ec2_nexus_instance_ami
+  ec2_instance_type = var.ec2_nexus_instance_type
+  security_group_ids = [module.ecs_k8s_security_groups.sg_ec2_k8s_id]
+  enable_public_ip = true
+  ebs_volume_size = 20
+  ebs_volume_type = "gp2"
+}
 
 // EC2 for jenkins
 # module "ec2_jenkins" {
